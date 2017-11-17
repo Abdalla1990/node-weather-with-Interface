ec071743e20a676d52c1b20a5d1e3f4c0d292f62
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2017-11-17 14:10:21 -0500

    test

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Address');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/giver-trail');
 module.exports = { mongoose };
